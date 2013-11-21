b3abedfbdaface4e152135bd5df726f01177a703
Ephraim <ephraim@tvtak.com>
2013-11-21 18:27:35 +0200

    made OpenCV properly link with Buildyard FFmpeg and not the system FFmpeg

--- build_linux.sh
+++ build_linux.sh
@@ -9,5 +9,7 @@ fi
 build_dir=`readlink -m $1`
 
 PATH=$PATH:$build_dir/3rdPartyLibs/Buildyard/install/bin
+export LIBRARY_PATH=$build_dir/3rdPartyLibs/Buildyard/install/lib
+export PKG_CONFIG_PATH=$build_dir/3rdPartyLibs/Buildyard/install/lib/pkgconfig
 
-make -C `dirname $0` BUILD=$build_dir/3rdPartyLibs/Buildyard CMakeArgs='-DEXTERNAL_PACKAGES_SOURCE_DIR=BINARY_DIR' Boost OpenCV FFmpeg
+make -C `dirname $0` BUILD=$build_dir/3rdPartyLibs/Buildyard CMakeArgs='-DEXTERNAL_PACKAGES_SOURCE_DIR=BINARY_DIR' Boost OpenCV
